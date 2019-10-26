c48704811ba0046e849b6e1b30d2ad6f104e7fc5
Rob Morgan <robbym@gmail.com>
2019-10-26 16:18:39 +0200

    support custom ports

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 
 // Constants
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const HOST = "0.0.0.0";
 
 // App
 const app = express();
 app.get("/", (req, res) => res.send("Hello World!"));
 
-var server = app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+let server = app.listen(PORT, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
 
 module.exports = server;
