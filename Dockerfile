ea6e5481ce05439af902626812e56d1e4f393055
Rob Morgan <robbym@gmail.com>
2019-10-27 18:16:10 +0100

    prefer port 80 for production

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,5 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 
-EXPOSE 8080
+EXPOSE 80
 CMD [ "npm", "start" ]
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 // Constants
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 const HOST = "0.0.0.0";
 
 // App
